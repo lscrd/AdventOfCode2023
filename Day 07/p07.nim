75f5230a7374806af3c84ed422598db05bfdd0f0
Luc Secouard <14235161+lscrd@users.noreply.github.com>
2023-12-08 19:03:14 +0100

    Add files via upload

--- Day 07/p07.nim
+++ Day 07/p07.nim
@@ -1,7 +1,15 @@
 import std/[algorithm, strscans, sugar, tables]
 
+# We represent the cards by a value from 0 to 12. This allows direct
+# comparison without using a custom comparison function.
+#
+# Another solution could be to represent cards with an enumeration type
+# (different for each part) but to avoid defining two procedures to build
+# the data representation, we had either to define a generic function or
+# to use inheritance.
+
 type
-  Card = char
+  CardValue = 0..12
   HandType {.pure.} = enum HighCard
                            OnePair
                            TwoPairs
@@ -9,33 +17,35 @@ type
                            FullHouse
                            FourOfAKind
                            FiveOfAKind
-  Hand = array[5, Card]
+  Hand = array[5, CardValue]
   HandAndBid = tuple[hand: Hand; bid: int]
 
-# Read the hands and the associated bids.
-var input: seq[HandAndBid]
-for line in lines("p07.data"):
-  var handStr: string
-  var bid: int
-  if line.scanf("$+ $i", handStr, bid):
-    var hand: Hand
-    for i, c in handStr:
-      doAssert c in "23456789TJQKA"
-      hand[i] = c
-    input.add (hand, bid)
+const
+  # Mappings from card character to card value.
+  Values1 = collect(for i, c in "23456789TJQKA": {c: i})  # For part 1.
+  Values2 = collect(for i, c in "J23456789TQKA": {c: i})  # For part 2.
+  Joker = Values2['J']   # Joker value in part 2.
+
+var
+  input: seq[HandAndBid]
+  result: int
+
+proc readHandsAndBids(filename: string; values: Table[char, int]): seq[HandAndBid] =
+  ## Read the hands and the associated bids.
+  for line in lines(filename):
+    var handStr: string
+    var bid: int
+    if line.scanf("$+ $i", handStr, bid):
+      var hand: Hand
+      for i, c in handStr:
+        if c notin values:
+          raise newException(ValueError, "Illegal card: " & c)
+        hand[i] = values[c]
+      result.add (hand, bid)
 
 
 
 ### Part 1 ###
-
-# Table used for comparison of cards.
-const Values1 = collect(for i, c in "23456789TJQKA": {c: i})
-
-
-func cmp1(card1, card2: Card): int =
-  ## Compare two cards.
-  cmp(Values1[card1], Values1[card2])
-
 
 func handType1(hand: Hand): HandType =
   ## Return the hand type of a hand using part 1 rules.
@@ -68,28 +78,21 @@ func cmp1(hb1, hb2: HandAndBid): int =
   if result == 0:
     # Same hand type. Compare cards.
     for i in 0..4:
-      result = cmp1(hb1.hand[i], hb2.hand[i])
+      result = cmp(hb1.hand[i], hb2.hand[i])
       if result != 0: return
 
-# Compute result.
-input.sort(cmp1)    # Sort the hands with associated bids.
-var result = 0
+
+input = readHandsAndBids("p07.data", Values1)
+input.sort(cmp1)    # Sort the hands with associated bids using rules 1.
+result = 0
 for i, hb in input:
   result += hb.bid * (i + 1)
+
 echo "Part 1: ", result
 
 
 
 ### Part 2 ###
-
-# Table used for comparison of cards.
-const Values2 = collect(for i, c in "J23456789TQKA": {c: i})
-
-
-func cmp2(card1, card2: Card): int =
-  ## Compare two cards.
-  cmp(Values2[card1], Values2[card2])
-
 
 func handType2(hand: Hand): HandType =
   ## Return the hand type of a hand using part 2 rules.
@@ -98,7 +101,7 @@ func handType2(hand: Hand): HandType =
   result = HighCard
   var jokerCount = 0
   for card, count in hand.pairs:
-    if card == 'J':
+    if card == Joker:
       # Ignore jokers for now.
       jokerCount = count
       continue
@@ -144,11 +147,12 @@ func cmp2(hb1, hb2: HandAndBid): int =
   if result == 0:
     # Same hand type. Compare cards.
     for i in 0..4:
-      result = cmp2(hb1.hand[i], hb2.hand[i])
+      result = cmp(hb1.hand[i], hb2.hand[i])
       if result != 0: return
 
-# Compute result.
-input.sort(cmp2)    # Sort the hands with associated bids.
+
+input = readHandsAndBids("p07.data", Values2)
+input.sort(cmp2)    # Sort the hands with associated bids using rules 2.
 result = 0
 for i, hb in input:
   result += hb.bid * (i + 1)
